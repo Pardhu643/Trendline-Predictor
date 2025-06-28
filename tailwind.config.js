3c18043c3dbc4efc7126d7a7aff1adc59cb42cc3
Your Name <your_real_email@example.com>
2025-06-28 12:40:15 +0530

    Initial project commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",       // Your main HTML file
+    "./src/**/*.js"       // Only JS files in src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
